75e6789190cdf9a898dc5c4f8480f47de10d7984
Dosia77 <125308879+Dosia77@users.noreply.github.com>
2023-02-25 02:04:39 +0300

    added task_2

---
+++ Task_2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks__2023
+{
+    internal class Task_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            int number_1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int number_2 = int.Parse(Console.ReadLine());
+            int max = 0;
+            if (number_1 > number_2)
+            {
+                max = number_1;
+            }
+            else
+            {
+                max = number_2;
+            }
+            Console.WriteLine("max = " + max);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
