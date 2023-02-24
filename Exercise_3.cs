64abce2a41f03b996d63e6c2fd978884532e0551
Dosia77 <125308879+Dosia77@users.noreply.github.com>
2023-02-25 02:25:58 +0300

    added task3

---
+++ Exercise_3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks__2023
+{
+    internal class Exercise_3
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+                Console.WriteLine("Да");
+            else
+                Console.WriteLine("Нет");
+            Console.ReadLine();
+        }
+    }
+}
