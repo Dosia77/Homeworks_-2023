75589afa328fa75d0510ab74b1577e2e4b3121be
Dosia77 <125308879+Dosia77@users.noreply.github.com>
2023-02-25 02:35:21 +0300

    added task_4

---
+++ Exercise_4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks__2023
+{
+    internal class Exercise_4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число больше единицы: ");
+            int a = int.Parse(Console.ReadLine());
+
+
+            int i = 0;
+            while (i < a)
+            {
+                i++;
+                if (i % 2 == 0)
+                {
+
+                    Console.Write(i + " ");
+
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
