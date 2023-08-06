26ce0b61434b544a64155c787751d0e832a487e7
Dosia77 <125308879+Dosia77@users.noreply.github.com>
2023-08-06 16:02:08 +0300

    added changes

--- Seminar5_3.cs
+++ Seminar5_3.cs
@@ -48,7 +48,8 @@ namespace Homeworks__2023
         }
         static void Main(string[] args)
         {
-            // задача # 38
+            // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+            //[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 
             int length = RequestDouble("Задайте длину массива: ");
             double[] arrayDouble = DoubleArray(length);
